d5908be6ea34b19a30b5973f30ee56c0a9590a2e
abebutel <41986140+abebutel@users.noreply.github.com>
2026-05-11 22:06:44 +0300

    Fix: Switch to Next.js for Vercel

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'app.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.write(html);
+  res.end();
+}
